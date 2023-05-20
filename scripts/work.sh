79682a35c0f6a2b5cc5b0a5b1de3fa689ad3b03e
einKnie <einKnie@gmx.at>
2023-05-21 00:35:29 +0200

    sceripts/work.sh: update ws name generation
    when opening a new work workspace, check if
    there are existing workspaces of the desired name.
    The list of existing workspaces is filtered to contain
    only workspaces which names start with the desired name,
    the number of found workspaces id incremented and
    appended to the new workspace name.
    
    e.g. one ws named "bla" exists, new ws "bla" will be called "bla2"
    
    Signed-off-by: einKnie <einKnie@gmx.at>

--- scripts/work.sh
+++ scripts/work.sh
@@ -27,25 +27,42 @@ default_name="$(basename "$default_dir")"
 ws_path="$default_dir"
 ws_name="$defaut_name"
 
-get_path_from_dir() {
-    # use rofi for directory listing
-    # return 1 on invalid path
+get_path() {
+    # list subdirs of the default_dir path and feed that to rofi for
+    # simple selection
     d="$(fd --search-path "$default_dir" --type=d --exact-depth=1 | rofi -dmenu -p "dir")"
-    echo "$d"
     [[ -d "$d" ]] || return 1
 
     ws_path="$d"
 }
 
+get_ws_name() {
+    # using basename here allows me to simply feed the given path to this function.
+    # if basename is presented with a non-path, e.g. "testname", it simply returns the input
+    # so this changes nothing if the function is called with the desired name directly
+    name=$(basename "$1")
 
-if ! get_path_from_dir ; then
+    # check how many workpaces exist starting with the given name
+    no=$(i3-msg -t get_workspaces | jq --arg nm "$name" '.[] | select(.name|startswith($nm)).id' | wc -l)
+    if [ $no -gt 0 ] ; then
+        # inc number and add to name (e.g. ws "bla" exists -> new ws: "bla2")
+        # btw, this is not perfect. if i have bla, bla1, bla2 and delete bla1 -> next ws will try bla2, which exists
+        # but i don't care enough atm
+        name="${name}$((no + 1))"
+    fi
+
+    echo "$name"
+}
+
+####################################
+
+# fetch desired cwd from user and name workspace accordingly
+if ! get_path ; then
     notify-send "invalid path"
     exit 1
 fi
-ws_name=$(basename "$ws_path")
 
-# set variables to use by caller
-workspace="$ws_name"
+workspace="$(get_ws_name "$ws_path")"
 
 cmds[0]="urxvt -name git -cd \"$ws_path\""
 cmds[1]="urxvt -name misc -cd \"$ws_path\""
