ff3322c1ca2aef71d601214cdef00f9b88f86c17
einKnie <einKnie@gmx.at>
2024-04-27 19:16:32 +0200

    scripts/monitoring.sh: more journal lines from start

--- scripts/monitoring.sh
+++ scripts/monitoring.sh
@@ -20,7 +20,7 @@ declare -a cmds
 workspace=""
 
 # commands
-cmds[0]="urxvt -name journal -e bash -c \"journalctl -fn 50\""
+cmds[0]="urxvt -name journal -e bash -c \"journalctl -fn 100\""
 cmds[1]="urxvt -name htop -e bash -c \"htop\""
 cmds[2]="urxvt -name vnstat -e bash -c \"vnstat --live\""
 cmds[3]="urxvt -name bash -e bash -c \"bash\""
